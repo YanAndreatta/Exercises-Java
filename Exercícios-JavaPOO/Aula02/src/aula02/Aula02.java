260a614cbd1cc10dd37b08afb3bb2d463a76c793
Yan Andreatta <andreattadelima@gmail.com>
2020-05-18 15:52:20 -0300

    Exercícios POO

---
+++ Exercícios-JavaPOO/Aula02/src/aula02/Aula02.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula02;
+
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f; 
+        c1.tampar();
+        //c1.tampada = false; // chamada de atributo
+        c1.status(); // chamada de método
+        c1.rabiscar();
+        
+        Caneta c2 = new Caneta();
+        c2.modelo = "Hostnet";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
+        
+    }
+    
+}
---
+++ Exercícios-JavaPOO/Aula04/src/aula04/Caneta02FeitoComALTINSERT.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula04;
+
+/**
+ *
+ * @author yandu
+ */
+public class Caneta02FeitoComALTINSERT {
+    private String modelo;
+    private float ponta;
+    private String cor;
+    private boolean tampada;
+    
+    public Caneta02FeitoComALTINSERT (String modelo, float ponta, String cor) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+    
+}
