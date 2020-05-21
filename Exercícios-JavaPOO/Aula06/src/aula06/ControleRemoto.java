d973654afecd4c05fb57a95d3304116b1e6f7091
Yan Andreatta <andreattadelima@gmail.com>
2020-05-21 13:43:07 -0300

    Exércicio Encapsulamento

---
+++ Exercícios-JavaPOO/Aula06/src/aula06/ControleRemoto.java
@@ -0,0 +1,92 @@
+package aula06;
+public class ControleRemoto implements Controlador {
+    //Atributos
+    
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+    
+    //Métodos Especiais 
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean getLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean getTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+    //Métodos Abstratos
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligdo? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.println("Volume: " + this.getVolume());
+        for (int i = 0; i <= this.getVolume(); i+=10) {
+            System.out.println("|");
+
+}
+    }
+
+    @Override
+    public void fecharMenu() {
+    }
+
+    @Override
+    public void maisVolume() {
+    }
+
+    @Override
+    public void menosVolume() {
+    }
+
+    @Override
+    public void ligarMudo() {
+    }
+
+    @Override
+    public void desligarMudo() {
+    }
+
+    @Override
+    public void play() {
+    }
+
+    @Override
+    public void pause() {
+    }
+    
+    
+}
