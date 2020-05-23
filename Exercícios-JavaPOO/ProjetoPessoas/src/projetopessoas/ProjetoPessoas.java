13674479709947fddabfda94bdf63fdd84839798
Yan Andreatta <andreattadelima@gmail.com>
2020-05-23 11:08:46 -0300

    Update ProjetoPessoas.java

--- Exercícios-JavaPOO/ProjetoPessoas/src/projetopessoas/ProjetoPessoas.java
+++ Exercícios-JavaPOO/ProjetoPessoas/src/projetopessoas/ProjetoPessoas.java
@@ -30,6 +30,13 @@ public class ProjetoPessoas {
         p4.setSexo("F");
         p2.setIdade(18);
         
+        p2.setCurso("Informática");
+        p3.setSalario(2500.75f);
+        p4.setSetor("Estoque");
+        
+        //p1.receberAumento(550.20f);
+        //p2.mudarTrabalho();
+        //p4.cancelarMatr();
         
         System.out.println(p1.toString());
         System.out.println(p2.toString());
