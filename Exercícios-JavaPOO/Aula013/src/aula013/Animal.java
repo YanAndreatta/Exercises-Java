90f88999205d7fc872387ab78f13ad7a04b2b0f3
Yan Andreatta <andreattadelima@gmail.com>
2020-05-28 18:29:26 -0300

    Exercise java POO

---
+++ Exercícios-JavaPOO/Aula013/src/aula013/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula013;
+
+/**
+ *
+ * @author yandu
+ */
+public abstract class Animal {
+    protected float peso;
+    protected int idade;
+    protected int membros;
+    
+    public abstract void emitirSom();
+}
---
+++ Exercícios-JavaPOO/Aula013/src/aula013/Aula013.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula013;
+
+/**
+ *
+ * @author yandu
+ */
+public class Aula013 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Programa principal 
+        Cachorro c = new Cachorro();
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
+    }
+    
+}
