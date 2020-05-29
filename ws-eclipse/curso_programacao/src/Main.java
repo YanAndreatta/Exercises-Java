b69d3b7de3afcfe28fb56326dad62c2fc39d24b5
Yan Andreatta <andreattadelima@gmail.com>
2020-05-29 20:15:23 -0300

    Exercise

--- ws-eclipse/curso_programacao/src/Main.java
+++ ws-eclipse/curso_programacao/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			
+			/*
 			int y = 32;
 			double x = 10.35784;
 			
@@ -15,8 +17,27 @@ public class Main {
 			
 			System.out.println("Resultado = " + x + " Metros ");
 			System.out.printf("Resultado = %.2f metros%n", x);
+			*/
 			
+		
+		
+			String nome = "Maria";
+			int idade = 31;
+			double renda = 4000.0;
+			System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda); 
 			
+			/*
+			 * %f = ponto flutuante
+			 * 
+			 * %d = inteiro
+			 * 
+			 * %s = texto 
+			 * 
+			 * %n = quebra de linha
+			 * 
+			 */
+			
+		
 			
 			//System.out.println(y);
 		
