4e0d7317cf6757ec03fb98d097983ffd1b2bce97
Yan Andreatta <andreattadelima@gmail.com>
2020-05-29 19:51:40 -0300

    Exercise

--- ws-eclipse/curso_programacao/src/Main.java
+++ ws-eclipse/curso_programacao/src/Main.java
@@ -1,9 +1,28 @@
+import java.util.Locale;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Hello, world!");
+			int y = 32;
+			double x = 10.35784;
+			
+			System.out.printf("%.2f%n", x);
+			System.out.printf("%.4f%n", x);
+			Locale.setDefault(Locale.US);
+			
+			
+			
+			System.out.println("Resultado = " + x + " Metros ");
+			System.out.printf("Resultado = %.2f metros%n", x);
+			
+			
+			
+			//System.out.println(y);
+		
+			//System.out.println("Hello, world!");
+			//System.out.println("Bom dia!");
+			
 			
 	}
 
