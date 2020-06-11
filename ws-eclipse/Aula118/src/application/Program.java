fb372b0a2813c5f116d53c0bbd9b0da0ced9c3d3
Yan Andreatta <andreattadelima@gmail.com>
2020-06-10 22:33:33 -0300

    Terminando exercise - Aula118

--- ws-eclipse/Aula118/src/application/Program.java
+++ ws-eclipse/Aula118/src/application/Program.java
@@ -1,10 +1,55 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-
+		
+		Scanner t = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+	
+		System.out.print("Enter Cliente data:");
+		System.out.print("Name: ");
+		String clientName = t.nextLine();
+		System.out.print("Email: ");
+		String email = t.next();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDay = sdf.parse(t.next());
+		System.out.print("Enter order data:");
+		System.out.printf("Status: ");
+		String status = t.nextLine();
+		System.out.print("How many items to this order? ");
+		int n = t.nextInt();
+		
+		Order order1 = new Order(sdf(), OrderStatus.valueOf(status), new Client(clientName, email, birthDay));
+		
+		for(int i = 0; i < n; i++) {
+			
+		}
+		
+		
+		
+		t.close();
+		
 	}
+	
+	/*public static Date sdf() {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		Date x1 = new Date(System.currentTimeMillis());
+		Date x2 = Date.from(Instant.parse(sdf.format(x1)));
+		return x2;
+	}*/
 
 }
---
+++ ws-eclipse/Aula118/src/entities/OrderItem.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class OrderItem {
+	
+	private Integer quantity;
+	private Double price;
+	
+	private Product product;
+	
+	//Constructors
+	public OrderItem() {
+	}
+
+	public OrderItem(Integer quantity, Double price, Product product) {
+		this.quantity = quantity;
+		this.price = price;
+		this.product = product;
+	}
+
+	//Getters and Setters
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	//methods
+	
+	public double subTotal(double subTotal) {
+		return subTotal;
+	}
+	
+}
